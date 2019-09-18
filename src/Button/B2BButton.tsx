699e91ba8866e06e49be32a98425a0e502290dec
cescoferraro <francescoaferraro@gmail.com>
2019-09-18 00:47:43 -0300

    better structure

--- src/Button/B2BButton.tsx
+++ src/Button/B2BButton.tsx
@@ -1,57 +1,14 @@
 import * as React from 'react';
 import {Button} from "@material-ui/core";
-import {createStyles, makeStyles} from "@material-ui/styles";
 import PropTypes from "prop-types";
-
-let regular = "primary";
-let defaultProps = {
-  submit: false,
-  style: {},
-  onClick: () => {},
-  type: regular,
-  disabled: false
-};
-
-const B2BButtonTypes: any = [regular, "secondary"];
-
-let propTypes = {
-  submit: PropTypes.bool,
-  style: PropTypes.object,
-  disabled: PropTypes.bool,
-  label: PropTypes.string.isRequired,
-  type: PropTypes.oneOf(B2BButtonTypes),
-  onClick: PropTypes.func
-};
-
-const ya = (type: string) => makeStyles(() => {
-  let background = "#0098A6";
-  let sbackground = "white";
-  let hoverbackground = "#006a74";
-  let shoverbackground = "#b2b2b2";
-  let textColor = "white";
-  let stextColor = background;
-  return createStyles({
-    root: {
-      minHeight: 50,
-      minWidth: 300,
-      background: type === regular ? background : sbackground,
-      color: type === regular ? textColor : stextColor,
-      "&:hover": {
-        color: type === regular ? textColor : stextColor,
-        background: type === regular ? hoverbackground : shoverbackground
-      }
-    },
-    containedSecondary: {
-      boxShadow: "none",
-    }
-  });
-});
+import {makeStylesGen} from "./styles";
+import {defaultProps, propTypes} from "./props";
 
 export const B2BButton = (
-  {type, disabled, style, submit, label, onClick}:
-    any
+  {type, disabled, style, submit, label,onClick}:
+    PropTypes.InferProps<typeof propTypes>
 ) => {
-  const classes: any = ya(type)();
+  const classes: any = makeStylesGen(type)();
   return (
     <Button
       classes={{
--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,5 @@
 export * from "./Button/B2BButton"
 export * from "./MainHeader/MainHeader"
+export {makeStylesGen} from "./Button/styles";
+export {propTypes} from "./Button/props";
+export {defaultProps} from "./Button/props";
