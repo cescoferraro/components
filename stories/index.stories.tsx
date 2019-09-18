d05cb9bcfbe79e3c436d8866160888e537695714
cescoferraro <francescoaferraro@gmail.com>
2019-09-17 21:36:10 -0300

    minor

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -3,6 +3,13 @@ import {storiesOf} from '@storybook/react';
 import {B2BButton} from '../src';
 import {select} from "@storybook/addon-knobs"
 
+export const flexer: React.CSSProperties = {
+  display: "flex",
+  justifyContent: "center",
+  alignSelf: "center",
+  alignItems: "center"
+};
+
 const typeGEN = (type: string) =>
   select("Type", {primary: "primary", secondary: "secondary"}, type);
 
@@ -14,11 +21,13 @@ storiesOf('B2BButton', module)
     />
   )
   .add('secondary', () =>
-    <B2BButton
-      type={typeGEN("secondary")}
-      label={"CLICK ME"}
-      onClick={() => {
-        console.log(2378324)
-      }}
-    />
+    <div style={{...flexer, minHeight:200}}>
+      <B2BButton
+        type={typeGEN("secondary")}
+        label={"CLICK ME"}
+        onClick={() => {
+          console.log(2378324)
+        }}
+      />
+    </div>
   );
